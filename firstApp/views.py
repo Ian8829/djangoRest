7d55976fedcd85e3cb69c0f52fdcee730346e87d
ghjkghj <ghjkghj@naver.com>
2021-12-20 17:19:34 +0900

    use database

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from firstApp.models import Employee
 
 def employeeView(request):
-  emp = {
-    'id': 123,
-    'name': 'John',
-    'sal': 1000000,
+  data = Employee.objects.all()
+  response = {
+    'employees': list(data.values('name', 'sal'))
   }
 
-  return JsonResponse(emp)
+  return JsonResponse(response)
